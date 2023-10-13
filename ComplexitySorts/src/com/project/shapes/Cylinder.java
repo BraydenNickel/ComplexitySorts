8807d4b07cfaf5fd6e9a7fabd9e3e6171c4b0ca0
BraydenNickel <120237434+BraydenNickel@users.noreply.github.com>
2023-10-12 22:44:47 -0600

    Update Cylinder.java

--- ComplexitySorts/src/com/project/shapes/Cylinder.java
+++ ComplexitySorts/src/com/project/shapes/Cylinder.java
@@ -1,5 +1,21 @@
 package com.project.shapes;
 
-public class Cylinder {
+public class Cylinder extends ThreeDimensionalShape {
+	double radius;
+	
+	public Cylinder(double height, double radius) {
+		super(height);
+		this.radius = radius;
+	}
+	
+	@Override
+	public double calculateBaseArea() {
+		return Math.PI * radius * radius;
+	}
+	
+	@Override
+	public double calculateVolume() {
+		return Math.PI * (radius * radius) * height;
+	}
 
 }
