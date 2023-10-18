3b3aa3e912a68dbccc42cda26f5cb6c648f0f0ce
BraydenNickel <120237434+BraydenNickel@users.noreply.github.com>
2023-10-18 09:32:53 -0600

    Update OctaPrism.java

--- ComplexitySorts/src/com/project/shapes/OctaPrism.java
+++ ComplexitySorts/src/com/project/shapes/OctaPrism.java
@@ -1,5 +1,16 @@
 package com.project.shapes;
 
-public class OctaPrism {
-
+public class OctaPrism extends Prism {
+	
+	public OctaPrism(double height, double sideLength) {
+		super(height, sideLength);
+	}
+	@Override
+	public double calculateBaseArea() {
+		return 2 * (1 + Math.sqrt(2)) * sideLength * sideLength;
+	}
+	@Override
+	public double calculateVolume() {
+		return(sideLength * sideLength * height) / 4 ;
+	}
 }
