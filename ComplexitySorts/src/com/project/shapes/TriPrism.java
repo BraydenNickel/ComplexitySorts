324ba318b7ee6fba4c9a5e6757e7ccc63ea02b19
BraydenNickel <120237434+BraydenNickel@users.noreply.github.com>
2023-10-20 16:21:45 -0600

    Update TriPrism.java

--- ComplexitySorts/src/com/project/shapes/TriPrism.java
+++ ComplexitySorts/src/com/project/shapes/TriPrism.java
@@ -2,7 +2,7 @@ package com.project.shapes;
 
 public class TriPrism extends Prism{
 	public TriPrism(double height, double sideLength) {
-		super(height, sideLength);
+		super("Trianglar Prism", height, sideLength);
 	}
 	
 	@Override
@@ -13,11 +13,6 @@ public class TriPrism extends Prism{
 	@Override
 	public double calculateVolume() {
 		return (sideLength * sideLength * height) / 2;
-	}
-	
-	@Override
-	public String toString() {
-	    return "Triangular Prism with height: " + height + " and side length: " + sideLength;
 	}
 
 }
