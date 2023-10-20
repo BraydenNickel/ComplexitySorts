9d20695cf6b9de3c9e91df7e6d8f16c7f9b37aec
BraydenNickel <120237434+BraydenNickel@users.noreply.github.com>
2023-10-19 19:56:28 -0600

    Update Pyramid.java

--- ComplexitySorts/src/com/project/shapes/Pyramid.java
+++ ComplexitySorts/src/com/project/shapes/Pyramid.java
@@ -1,22 +1,27 @@
 package com.project.shapes;
 
-public class PentaPrism extends Prism {
-    public PentaPrism(double height, double sideLength) {
-		super(height, sideLength);
+public class Pyramid extends ThreeDimensionalShape{
+	double sideLength;
+	
+	public Pyramid(double height, double sideLength) {
+		super(height);
+		this.sideLength = sideLength;
 	}
-
-    @Override
-    public double calculateBaseArea() {
-        return (5 * sideLength * sideLength * Math.tan(Math.toRadians(54))) / 4;
-    }
-
-    @Override
-    public double calculateVolume() {
-        return (sideLength * sideLength * height);
-    }
-    
-    @Override
+	
+	@Override
+	public double calculateBaseArea() {
+		return sideLength * sideLength;
+		
+	}
+	
+	@Override 
+	public double calculateVolume() {
+		return(sideLength* sideLength * height) / 3;
+	}
+	
+	@Override
 	public String toString() {
-	    return "Pentagonal Prism with height: " + height + " and side length: " + sideLength;
+	    return "Pyramid with height: " + height + " and side length: " + sideLength;
 	}
+
 }
