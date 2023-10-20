12f7464b8163bb408820f24cf6ccb4d8c73cfcfa
BraydenNickel <120237434+BraydenNickel@users.noreply.github.com>
2023-10-20 16:20:39 -0600

    Update Pyramid.java

--- ComplexitySorts/src/com/project/shapes/Pyramid.java
+++ ComplexitySorts/src/com/project/shapes/Pyramid.java
@@ -4,7 +4,7 @@ public class Pyramid extends ThreeDimensionalShape{
 	double sideLength;
 	
 	public Pyramid(double height, double sideLength) {
-		super(height);
+		super("Pyramid", height);
 		this.sideLength = sideLength;
 	}
 	
@@ -17,11 +17,6 @@ public class Pyramid extends ThreeDimensionalShape{
 	@Override 
 	public double calculateVolume() {
 		return(sideLength* sideLength * height) / 3;
-	}
-	
-	@Override
-	public String toString() {
-	    return "Pyramid with height: " + height + " and side length: " + sideLength;
 	}
 
 }
